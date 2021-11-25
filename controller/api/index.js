9cb500cda0cc0dddc823034d344a8a872d4bcca7
sdesire <sdesire@ltu.edu>
2021-11-24 21:09:48 -0500

    server/index update

---
+++ controller/api/index.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+
+const catergoryRoutes = require("./category-routes.js");
+
+const flashcardRoutes = require("./flashcard-routes.js");
+
+const userRoutes = require("./user-routes.js");
+
+router.use("/api", apiRoutes);
+
+router.use("/category", categoryRoutes);
+
+router.use("/dashboard", flashcardRoutes);
+
+router.use("/user", userRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
