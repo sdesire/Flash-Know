64f7d5f88d27f5dc4b2624b8e7c9ec9a8ba602e4
sdesire <sdesire@ltu.edu>
2021-11-26 12:48:21 -0500

    added index and user to api folder

---
+++ controller/api/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const categoryRoutes = require("./category-routes");
+
+const flashcardRoutes = require("./flashcard-routes");
+
+const userRoutes = require("./user-routes");
+
+router.use("/categorys", categoryRoutes);
+router.use("/flashcards", flashcardRoutes);
+router.use("/users", userRoutes);
+
+module.exports = router;
