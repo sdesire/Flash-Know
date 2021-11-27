9de5ed61602c281b18e93c78f4b0404436e81438
beamchristian <beamchristian@yahoo.com>
2021-11-26 21:58:04 -0500

    category-routes

---
+++ controller/api/category-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+
+const { Category } = require('../../models');
+
+const session = require('express-session');
+
+// find all categories
+router.get('/', (req, res) => {
+  Category.findAll()
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// create new category
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Category.create({
+      title: req.body.title,
+      user_id: req.body.user_id,
+    })
+      .then(dbCategoryData => res.json(dbCategoryData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+// delete category
+router.delete('/:id', withAuth, (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
