0fb03e948bb3860937e45b7615e688f71145931f
beamchristian <beamchristian@yahoo.com>
2021-12-01 17:33:15 -0500

    flashcard-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes.js');
 const userRoutes = require('./user-routes.js');
-// const flashcardRoutes = require('./flashcard-routes.js');
+const flashcardRoutes = require('./flashcard-routes.js');
 
 router.use('/categories', categoryRoutes);
 router.use('/users', userRoutes);
-// router.use('/flashcards', flashcardRoutes);
+router.use('/flashcards', flashcardRoutes);
 
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,113 +1,57 @@
-<<<<<<< HEAD
-// async function loginFormHandler(event) {
-//   event.preventDefault();
-
-//   const email = document.querySelector('#email-login').value.trim();
-//   const password = document.querySelector('#password-login').value.trim();
-
-//   if (email && password) {
-//     const response = await fetch('/api/users/login', {
-//       method: 'post',
-//       body: JSON.stringify({
-//         email,
-//         password,
-//       }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/dashboard/');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// }
-
-// async function signupFormHandler(event) {
-//   event.preventDefault();
-
-//   const username = document.querySelector('#username-signup').value.trim();
-//   const email = document.querySelector('#email-signup').value.trim();
-//   const password = document.querySelector('#password-signup').value.trim();
-
-//   if (username && email && password) {
-//     const response = await fetch('/api/users', {
-//       method: 'post',
-//       body: JSON.stringify({
-//         username,
-//         email,
-//         password,
-//       }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/dashboard/');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// }
-
-// document
-//   .querySelector('.login-form')
-//   .addEventListener('submit', loginFormHandler);
-
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
-=======
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/user/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/user', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
->>>>>>> 24731143428c11e84a1ab6abe7f6294daeabdf72
+}
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
