351972dad0c7181b5f383f54db315f0854b20ced
sdesire <sdesire@ltu.edu>
2021-12-01 12:15:05 -0500

    dashboard routes  and handlebars

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,97 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+
+const { Category, User, Flashcard } = require("../models");
+
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "created_at"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No flashcards found with this id" });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+      res.render("edit-flashcard", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edituser", withAuth, (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.session.user_id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+
+      const user = dbUserData.get({ plain: true });
+      res.render("edit-user", { user, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
