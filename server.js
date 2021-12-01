05029090838cd695c5c6be12fa0172317e59ff0d
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-30 20:51:10 -0500

    fixed files

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const path = require('path');
-const express = require('express');
- const exphbs = require('express-handlebars');
-
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sequelize = require('./config/connection');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
-  secret: 'Super secret',
+  secret: "Super secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -19,19 +15,14 @@ const sess = {
     db: sequelize,
   }),
 };
-
-
- app.use(express.static(path.join(__dirname, 'public')));
- app.engine("handlebars", hbs.engine);
- app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
- app.set('view engine', 'handlebars');
-
- app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+// app.engine("handlebars", hbs.engine);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(require('./controllers'));
-
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.use(require("./controllers"));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
