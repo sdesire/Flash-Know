9e424ccebe8cab9f9e97cec112560a747988de94
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-30 19:34:27 -0500

    pulled current code

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: {{name}}");
+const exphbs = require("express-handlebars");
+const { appendFile } = require("fs");
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 console.log(template({ name: "Flashcards" }));
 
 var source = document.getElementById("entry-template").innerHTML;
