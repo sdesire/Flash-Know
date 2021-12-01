b9278ff6a80ac312bc8c254fe180306e134245dc
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-30 19:51:46 -0500

    fixed pathing for css

--- server.js
+++ server.js
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-const exphbs = require("express-handlebars");
-const { appendFile } = require("fs");
-const hbs = exphbs.create({});
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-console.log(template({ name: "Flashcards" }));
-
-var source = document.getElementById("entry-template").innerHTML;
-var template = template(context);
-var context = {
-  title: "My New Flashcard App",
-  body: "This is our flashcard app!",
-};
-var html = template(context);
-
-//const path = require("path");
-=======
 const path = require('path');
 const express = require('express');
  const exphbs = require('express-handlebars');
->>>>>>> 391d9577134980e7e696e1a51aad343edba0abb2
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -52,40 +32,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(require('./controllers'));
 
-<<<<<<< HEAD
-//const hbs = exphbs.create({ helpers });
-
-//const sess = {
-//secret: process.env.DB_SESSION_SECRET,
-// cookie: { maxAge: 7200000 },
-//resave: false,
-//saveUninitialized: true,
-//store: new SequelizeStore({
-//db: sequelize,
-// }),
-//};
-
-//const app = express();
-
-//const PORT = process.env.PORT || 3001;
-
-//app.use(express.static(path.join(__dirname, "public")));
-
-//app.engine("handlebars", hbs.engine);
-//app.set("view engine", "handlebars");
-
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
-
-//app.use(session(sess));
-
-//app.use(routes);
-
-//sequelize.sync({ force: false }).then(() => {
-//app.listen(PORT, () => console.log("Now listening"));
-//});
-=======
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
->>>>>>> 391d9577134980e7e696e1a51aad343edba0abb2
