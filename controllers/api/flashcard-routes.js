1018f18e04b7d37109f231158c2df3bc03110338
beamchristian <beamchristian@yahoo.com>
2021-12-01 20:01:57 -0500

    bug

--- controllers/api/flashcard-routes.js
+++ controllers/api/flashcard-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Flashcard, Category } = require('../../models');
+const { Flashcard } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // find all flashcards
@@ -13,8 +13,9 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-  Category.create({
-    flashcard_text: req.body.flashcard_text,
+  Flashcard.create({
+    question: req.body.question,
+    answer: req.body.answer,
     user_id: req.body.user_id,
     category_id: req.body.category_id,
   })
@@ -22,6 +23,25 @@ router.post('/', withAuth, (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  Flashcard.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(dbFlashcardData => {
+      if (!dbFlashcardData) {
+        res.status(404).json({ message: 'No flashcard found with this id!' });
+        return;
+      }
+      res.json(dbFlashcardData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
