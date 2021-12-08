bc9dbca809f9c5e43276f02e889023dab65bdec7
beamchristian <beamchristian@yahoo.com>
2021-12-08 17:00:12 -0500

    update 2

--- public/javascript/add-card.js
+++ public/javascript/add-card.js
@@ -1,17 +1,40 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const category_id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
+  const title = document.querySelector('input[name="category-title"]').value;
+  const question = document.querySelector('input[name="flashcard-question"]')
+    .value;
+  const answer = document.querySelector('input[name="flashcard-answer"]').value;
 
+  const response = await fetch(`/api/categories`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   if (response.ok) {
+    console.log(response.body);
+    // const flashcardResponse = await fetch(`/api/flashcards`, {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     category_id,
+    //     question,
+    //     answer,
+    //   }),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // });
     document.location.replace('/dashboard');
   } else {
+    console.log('categories error');
     alert(response.statusText);
   }
 }
 
-// document
-//   .querySelector('.new-flashcard-form')
-//   .addEventListener('submit', newFormHandler);
+document
+  .querySelector('.new-flashcard-form')
+  .addEventListener('submit', newFormHandler);
